f4eade9477aeb8ee1c3c0b95f174989cf69074c8
frank <stipro150197@gmail.com>
2020-06-09 07:02:36 -0500

    Diseño de Comision (Modificación Inputs)

--- views/inventario/main.php
+++ views/inventario/main.php
@@ -11,9 +11,13 @@ $urlcurrent = $urlseparate[3];
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
     <title>Hello, <?php echo $urlcurrent?>!</title>
   </head>
 <body>
@@ -21,12 +25,154 @@ $urlcurrent = $urlseparate[3];
     //Llamo Nav
     include ('../nav.php');
 ?>
-    <h1>Hello, <?php echo $urlcurrent?>!</h1>
+<div class="container-fluid">
+  <div class="shadow-sm p-3 mb-2 bg-white rounded">
+    <h1>Bienvenido, <?php echo $urlcurrent?>!</h1>
+  </div>
 
+  <div class="card text-left">
+    <div class="card-header">
+    Consultar inventario
+    <a href="#" class="btn btn-primary">Agregar</a>
+    </div>
+    <div class="card-body">
+      <!--<h5 class="card-title">Special title treatment</h5>
+      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>-->
+      <form>
+        <div class="form-row">
+          <div class="col">
+            <!-- Almacen -->
+            <div class="form-group">                                        
+              <label for="filAlm"><strong>Almacen</strong></label>
+              <select id="filAlm" class="selectpicker form-control" data-live-search="true">
+                <option data-tokens="">Selecciona un Almacen</option>
+                <option data-tokens="Recepción">Recepción</option>
+                <option data-tokens="Sistemas">Sistemas</option>
+              </select>
+            </div>
+          </div>
+          <div class="col">
+            <!-- Producto -->
+            <div class="form-group">                                        
+              <label for="filCodNom"><strong>cód o nombre</strong></label>
+              <select id="filCodNom" class="selectpicker form-control" data-live-search="true">
+                <option data-tokens="">Selecciona o busque un Producto</option>
+                <option data-tokens="Lapicero">Lapicero</option>
+                <option data-tokens="Borrador">Borrador</option>
+                <option data-tokens="Papel">Papel</option>
+              </select>
+            </div>
+
+          </div>
+        </div>
+      </form>
+
+      <form>
+        <div class="row">
+          <div class="col-sm">
+            <!---->
+            <div class="form-group">                                        
+              <label for="filcat"><strong>Filtrar por categoria</strong></label>
+              <select id="filcat" class="selectpicker form-control" data-live-search="true">
+                <option data-tokens="">Selecciona una categoría</option>
+                <option data-tokens="Accesorios">Accesorios</option>
+                <option data-tokens="Equipos">Equipos</option>
+                <option data-tokens="Repuestos">Repuestos</option>
+              </select>
+            </div>
+
+          </div>
+          <div class="col-sm">
+            <!---->
+            <div class="form-group">                                        
+              <label for="filrango"><strong>Rango de Fecha</strong></label>
+              <input id="filrango" class="form-control"  type="text" name="daterange" value="01/01/2018 - 01/15/2018" />
+            </div>
+
+          </div>
+        </div>
+      </form>
+
+      <hr>
+
+      <div class="card-deck">
+        <!-- Item 1-->
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>
+      <!-- Item 2 -->
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>
+      <!-- Item 3 -->
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>
+      <!-- Item 4 -->
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>
+      </div>
+
+      
+    </div>
+    <div class="card-footer text-muted">
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+      </ul>
+    </nav>
+    </div>
+  </div>
+</div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+    
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+
+<script>
+  $(function() {
+    $('input[name="daterange"]').daterangepicker({
+      opens: 'left'
+    }, function(start, end, label) {
+      console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+  });
+</script>
 </body>
 </html>
