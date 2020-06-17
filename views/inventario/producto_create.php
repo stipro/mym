a6b82c58936de82c284e16f4ee550d28db371e5c
stipro <stipro150197@gmail.com>
2020-06-17 17:12:36 -0500

    Agregando diseño (kardex) y (creacion producto)

---
+++ views/inventario/producto_create.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+    <title>Productos</title>
+</head>
+<body>
+    <div class="container-fluid">
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Almacen</label>
+        <div>
+            <select class="form-control selectpicker" data-live-search="true">
+                <option data-tokens="Recepción">Recepción</option>
+                <option data-tokens="Informatica">Informatica</option>
+                <option data-tokens="Cural">Transporte (Cural)</option>
+            </select>
+        </div>
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Proveedor</label>
+        <div>
+            <select class="form-control selectpicker" data-live-search="true">
+                <option data-tokens="Recepción">Proveedor</option>
+                <option data-tokens="Informatica">Proveedor 2</option>
+                <option data-tokens="Cural">Proveedor 3</option>
+            </select>
+        </div>
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Categoria</label>
+        <div>
+            <select class="form-control selectpicker" data-live-search="true">
+                <option data-tokens="Recepción">Categoria</option>
+                <option data-tokens="Informatica">Categoria 2</option>
+                <option data-tokens="Cural">Categoria 3</option>
+            </select>
+        </div>
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Marca</label>
+        <div>
+            <select class="form-control selectpicker" data-live-search="true">
+                <option data-tokens="Recepción">Marca</option>
+                <option data-tokens="Informatica">Marca 2</option>
+                <option data-tokens="Cural">Marca 3</option>
+            </select>
+        </div>
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Codigo</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Codigo">
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Nombre</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nombre">
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Precio</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Precio">
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Stock</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Stock">
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Modelo</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Modelo">
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">n° Serial</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Modelo">
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">n° Lote</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Modelo">
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Garantia</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Garantia">
+    </div>
+    <div class="mb-3">
+        <div class="form-check form-switch">
+        <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+        <label class="form-check-label" for="flexSwitchCheckChecked">Estado Modulo</label>
+        </div>
+    </div>
+    <div class="mb-3">
+    <label for="exampleFormControlTextarea1" class="form-label">Descripción</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+    </div>
+    <script src="./../../assets/js/jquery-3.4.1.min.js" type="text/javascript"></script>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+</body>
+</html>
