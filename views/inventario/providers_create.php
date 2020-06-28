0844ddd79e77a0c80efa0e54a140a86429f9bc4c
frank <stipro150197@gmail.com>
2020-06-28 12:20:54 -0500

    Diseño proveedor (Arreglar)

--- views/inventario/providers_create.php
+++ views/inventario/providers_create.php
@@ -14,48 +14,67 @@
             </button>
         </div>
         <div class="modal-body">
-        <div class="form-row">
-            <div class="col-sm-4 text-center">
+        <div class="row">
+            <div class="col-sm-3 text-center">
                 <div class="kv-avatar">
                     <div class="file-loading">
                         <input id="avatar-1" name="avatar-1" type="file" required>
                     </div>
                 </div>
                 <div class="kv-avatar-hint">
-                    <small>Select file < 1500 KB</small>
+                    <small>Tamaño Maximo 1500 KB</small>
                 </div>
             </div>
-            <div class="form-group col-sm">
-                <label for="proveedor_ruc" class="form-label">Ruc</label>
-                <input type="number" class="form-control" id="proveedor_ruc" placeholder="Ruc">
-            </div>
-            <div class="form-group col-sm">
-                <label for="proveedor_nombre" class="form-label">Nombre</label>
-                <input type="text" class="form-control" id="proveedor_nombre" placeholder="Nombre">
-            </div>
-            <!--
-            <div class="form-group col-sm">
-                <label for="razonsoc_proveedor" class="form-label">Razon Social</label>
-                <input type="text" class="form-control" id="razonsoc_proveedor" placeholder="Razon Social">
-            </div>
-            <div class="form-group col-sm">
-                <label for="direccion_proveedor" class="form-label">Dirección</label>
-                <input type="email" class="form-control" id="direccion_proveedor" placeholder="Dirección">
-            </div>-->
-        </div>
-
-        <div class="form-row">
-            <div class="form-group col-sm">
-                <label for="telefono_proveedor" class="form-label">Telefono*</label>
-                <input type="number" class="form-control" id="telefono_proveedor" placeholder="Telefono">
-            </div>
-            <div class="form-group col-sm">
-                <label for="celular_proveedor" class="form-label">Celular*</label>
-                <input type="email" class="form-control" id="celular_proveedor" placeholder="Celular">
-            </div>
-            <div class="form-group col-sm">
-                <label for="correo_proveedor" class="form-label">Correo</label>
-                <input type="email" class="form-control" id="correo_proveedor" placeholder="Correo">
+            <div class="col-sm">
+                <div class="form-row">
+                    <div class="col-sm-3">
+                        <div class="form-group">
+                            <label for="proveedor_ruc" class="form-label">Ruc</label>
+                            <input type="number" class="form-control" id="proveedor_ruc" placeholder="Ruc">
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="form-group">
+                            <label for="proveedor_nombre" class="form-label">Nombre</label>
+                            <input type="text" class="form-control" id="proveedor_nombre" placeholder="Nombre">
+                        </div>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label for="razonsoc_proveedor" class="form-label">Razon Social</label>
+                            <input type="text" class="form-control" id="razonsoc_proveedor" placeholder="Razon Social">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label for="direccion_proveedor" class="form-label">Dirección</label>
+                            <input type="text" class="form-control" id="direccion_proveedor" placeholder="Dirección">
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="form-row">
+                    <div class="col-sm">
+                        <div class="form-group">
+                            <label for="telefono_proveedor" class="form-label">Telefono*</label>
+                            <input type="number" class="form-control" id="telefono_proveedor" placeholder="Telefono">
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="form-group">
+                            <label for="celular_proveedor" class="form-label">Celular*</label>
+                            <input type="number" class="form-control" id="celular_proveedor" placeholder="Celular">
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="form-group">
+                            <label for="correo_proveedor" class="form-label">Correo</label>
+                            <input type="email" class="form-control" id="correo_proveedor" placeholder="Correo">
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="mb-3">
@@ -65,8 +84,8 @@
             </div>
         </div>
         <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Descripción</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <label for="exampleFormControlTextarea1" class="form-label">Descripción</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
         </div>
       </div>
       <div class="modal-footer">
@@ -78,7 +97,7 @@
         <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
         </svg>
         Cerrar</button>
-        <button type="button" class="btn btn-primary">
+        <button id="btn-insert" type="button" class="btn btn-primary">
         <svg class="bi bi-pencil" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 0 1 1.414 0l2 2a1 1 0 0 1 0 1.414l-9 9a1 1 0 0 1-.39.242l-3 1a1 1 0 0 1-1.266-1.265l1-3a1 1 0 0 1 .242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z"/>
         <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 0 0 .5.5H4v.5a.5.5 0 0 0 .5.5H5v.5a.5.5 0 0 0 .5.5H6v-1.5a.5.5 0 0 0-.5-.5H5v-.5a.5.5 0 0 0-.5-.5H3z"/>
@@ -88,26 +107,9 @@
     </div>
   </div>
 </div>
+<script src="./../../assets/js/jquery-3.4.1.min.js" type="text/javascript"></script>
 <!-- the fileinput plugin initialization -->
 <script>
-var btnCust = '<button type="button" class="btn btn-secondary" title="Add picture tags" ' + 
-    'onclick="alert(\'Call your custom code here.\')">' +
-    '<i class="glyphicon glyphicon-tag"></i>' +
-    '</button>'; 
-$("#avatar-1").fileinput({
-    overwriteInitial: true,
-    maxFileSize: 1500,
-    showClose: false,
-    showCaption: false,
-    browseLabel: '',
-    removeLabel: '',
-    browseIcon: '<svg class="bi bi-folder-plus" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M9.828 4H2.19a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91H9v1H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181L15.546 8H14.54l.265-2.91A1 1 0 0 0 13.81 4H9.828zm-2.95-1.707L7.587 3H2.19c-.24 0-.47.042-.684.12L1.5 2.98a1 1 0 0 1 1-.98h3.672a1 1 0 0 1 .707.293z"/><path fill-rule="evenodd" d="M13.5 10a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1H13v-1.5a.5.5 0 0 1 .5-.5z"/><path fill-rule="evenodd" d="M13 12.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0v-2z"/></svg>',
-    removeIcon: '<i class="glyphicon glyphicon-remove"></i>',
-    removeTitle: 'Cancel or reset changes',
-    elErrorContainer: '#kv-avatar-errors-1',
-    msgErrorClass: 'alert alert-block alert-danger',
-    defaultPreviewContent: '<img src="/samples/default-avatar-male.png" alt="Your Avatar">',
-    layoutTemplates: {main2: '{preview} ' +  btnCust + ' {remove} {browse}'},
-    allowedFileExtensions: ["jpg", "png", "gif"]
-});
+
+
 </script>
