213c964fa0f779bb25229bb41fc5a19ca94933d5
stipro <stipro150197@gmail.com>
2020-08-13 18:01:10 -0500

    Tabla responsibe y mas ...

--- views/inventario/almacen.php
+++ views/inventario/almacen.php
@@ -95,6 +95,118 @@
       <hr>
       <div id="div_tabla">
       </div>
+      <div class="panel">
+					    <div class="panel-heading">
+					        <h3 class="panel-title">Sample Toolbar</h3>
+					    </div>
+					
+					    <!--Data Table-->
+					    <!--===================================================-->
+					    <div class="panel-body">
+					        <div class="pad-btm form-inline">
+					            <div class="row">
+					                <div class="col-sm-6 table-toolbar-left">
+					                    <button id="demo-btn-addrow" class="btn btn-purple"><i class="demo-pli-add"></i> Add</button>
+					                    <button class="btn btn-default"><i class="demo-pli-printer"></i></button>
+					                    <div class="btn-group">
+					                        <button class="btn btn-default"><i class="demo-pli-exclamation"></i></button>
+					                        <button class="btn btn-default"><i class="demo-pli-recycling"></i></button>
+					                    </div>
+					                </div>
+					                <div class="col-sm-6 table-toolbar-right">
+					                    <div class="form-group">
+					                        <input id="demo-input-search2" type="text" placeholder="Search" class="form-control" autocomplete="off">
+					                    </div>
+					                    <div class="btn-group">
+					                        <button class="btn btn-default"><i class="demo-pli-download-from-cloud"></i></button>
+					                        <div class="btn-group dropdown">
+					                            <button data-toggle="dropdown" class="btn btn-default dropdown-toggle">
+					                                <i class="demo-pli-gear"></i>
+					                                <span class="caret"></span>
+					                            </button>
+					                            <ul role="menu" class="dropdown-menu dropdown-menu-right">
+					                                <li><a href="#">Action</a></li>
+					                                <li><a href="#">Another action</a></li>
+					                                <li><a href="#">Something else here</a></li>
+					                                <li class="divider"></li>
+					                                <li><a href="#">Separated link</a></li>
+					                            </ul>
+					                        </div>
+					                    </div>
+					                </div>
+					            </div>
+					        </div>
+					        <div class="table-responsive">
+					            <table class="table table-striped">
+					                <thead>
+					                    <tr>
+					                        <th class="text-center">Invoice</th>
+					                        <th>User</th>
+					                        <th>Order date</th>
+					                        <th>Amount</th>
+					                        <th>Status</th>
+					                        <th>Tracking Number</th>
+					                    </tr>
+					                </thead>
+					                <tbody>
+					                    <tr>
+					                        <td><a class="btn-link" href="#"> Order #53431</a></td>
+					                        <td>Steve N. Horton</td>
+					                        <td><span class="text-muted"><i class="demo-pli-clock"></i> Oct 22, 2014</span></td>
+					                        <td>$45.00</td>
+					                        <td>
+					                            <div class="label label-table label-success">Paid</div>
+					                        </td>
+					                        <td>-</td>
+					                    </tr>
+					                    <tr>
+					                        <td><a class="btn-link" href="#"> Order #53432</a></td>
+					                        <td>Charles S Boyle</td>
+					                        <td><span class="text-muted"><i class="demo-pli-clock"></i> Oct 24, 2014</span></td>
+					                        <td>$245.30</td>
+					                        <td>
+					                            <div class="label label-table label-info">Shipped</div>
+					                        </td>
+					                        <td><i class="demo-pli-mine"></i> CGX0089734531</td>
+					                    </tr>
+					                    <tr>
+					                        <td><a class="btn-link" href="#"> Order #53433</a></td>
+					                        <td>Lucy Doe</td>
+					                        <td><span class="text-muted"><i class="demo-pli-clock"></i> Oct 24, 2014</span></td>
+					                        <td>$38.00</td>
+					                        <td>
+					                            <div class="label label-table label-info">Shipped</div>
+					                        </td>
+					                        <td><i class="demo-pli-mine"></i> CGX0089934571</td>
+					                    </tr>
+					                    <tr>
+					                        <td><a class="btn-link" href="#"> Order #53434</a></td>
+					                        <td>Teresa L. Doe</td>
+					                        <td><span class="text-muted"><i class="demo-pli-clock"></i> Oct 15, 2014</span></td>
+					                        <td>$77.99</td>
+					                        <td>
+					                            <div class="label label-table label-info">Shipped</div>
+					                        </td>
+					                        <td><i class="demo-pli-mine"></i> CGX0089734574</td>
+					                    </tr>
+					                    <tr>
+					                        <td><a class="btn-link" href="#"> Order #53435</a></td>
+					                        <td>Teresa L. Doe</td>
+					                        <td><span class="text-muted"><i class="demo-pli-clock"></i> Oct 12, 2014</span></td>
+					                        <td>$18.00</td>
+					                        <td>
+					                            <div class="label label-table label-success">Paid</div>
+					                        </td>
+					                        <td>-</td>
+					                    </tr>
+					                </tbody>
+					            </table>
+					        </div>
+					    </div>
+					    <!--===================================================-->
+					    <!--End Data Table-->
+					
+					</div>
     </div>
     <div class="card-footer text-muted">
     <nav aria-label="Page navigation example">
