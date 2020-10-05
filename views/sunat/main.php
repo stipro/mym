2b2f80fbd9e319227b580e5c901e64821bdd4cd0
stipro <stipro150197@gmail.com>
2020-10-05 18:01:53 -0500

    convertir archivo txt en object con js

--- views/sunat/main.php
+++ views/sunat/main.php
@@ -21,20 +21,20 @@ $urlcurrent = $urlseparate[3];
   </head>
 <body>
 <?php
-    //Llamo Nav
-    include ('../nav.php');
+  //Llamo Nav
+  include ('../nav.php');
 ?>
-    <h1>Hello, <?php echo $urlcurrent?>!</h1>
-    <button>Consultar Sunat</button>
-    <input value="ES" type="file" id="file-input" />
-    <h3>Contenido del archivo:</h3>
-    <pre id="contenido-archivo"></pre>
-    <?php
-    $alsunat = array();
-    $adsunar = array();
-    $c = 0;
+  <h1>Hello, <?php echo $urlcurrent?>!</h1>
+  <button type="button" id="btnConSunat" class="btn btn-success">Consultar Sunat</button>
+  <input class="btn btn-success" value="ES" type="file" id="file-input" />
+  <h3>Contenido del archivo:</h3>
+  <pre id="contenido-archivo"></pre>
+<?php
+  $alsunat = array();
+  $adsunar = array();
+  $c = 0;
     //URL DEL ARCHIVO
-    $ArchivoLeer = "./../../data/NDEBITO01_27092020.txt";
+    $ArchivoLeer = "./../../data/05102020.txt";
     if(touch($ArchivoLeer)){
       //
       $archivoID = fopen($ArchivoLeer, "r");
@@ -57,7 +57,8 @@ $urlcurrent = $urlseparate[3];
       fclose($archivoID);
     }
     //var_dump($alsunat);
-    $jelsunat = json_encode($alsunat);    
+    $jelsunat = json_encode($alsunat);
+    var_dump($alsunat);
     ?>
     <div class="csaSunat">
 
@@ -88,47 +89,6 @@ $urlcurrent = $urlseparate[3];
                     <td role="cell">Chief Sandwich Eater</td>
                     <td role="cell">Chief Sandwich Eater</td>
                 </tr>
-                <tr role="row">
-                    <th scope="row">2</th>
-                    <td role="cell">The</td>
-                    <td role="cell">Tick</td>
-                    <td role="cell">Crimefighter Sorta</td>
-                    <td role="cell">The</td>
-                    <td role="cell">Tick</td>
-                    <td role="cell">Crimefighter Sorta</td>
-                    <td role="cell">Crimefighter Sorta</td>
-                </tr>
-                <tr role="row">
-                    <th scope="row">3</th>
-                    <td role="cell">Jokey</td>
-                    <td role="cell">Smurf</td>
-                    <td role="cell">Giving Exploding Presents</td>
-                    <td role="cell">Jokey</td>
-                    <td role="cell">Smurf</td>
-                    <td role="cell">Giving Exploding Presents</td>
-                    <td role="cell">Giving Exploding Presents</td>
-                </tr>
-                <tr role="row">
-                    <th scope="row">4</th>
-                    <td role="cell">Cindy</td>
-                    <td role="cell">Beyler</td>
-                    <td role="cell">Sales Representative</td>
-                    <td role="cell">Cindy</td>
-                    <td role="cell">Beyler</td>
-                    <td role="cell">Sales Representative</td>
-                    <td role="cell">Sales Representative</td>
-                </tr>
-                <tr role="row">
-                    <th scope="row">5</th>
-                    <td role="cell">Captain</td>
-                    <td role="cell">Cool</td>
-                    <td role="cell">Tree Crusher</td>
-                    <td role="cell">Captain</td>
-                    <td role="cell">Cool</td>
-                    <td role="cell">Tree Crusher</td>
-                    <td role="cell">Tree Crusher</td>
-                    <!--<td role="cell">Under the couch</td>-->
-                </tr>
             </tbody>
             <tfoot>
                 <tr>
@@ -146,54 +106,100 @@ $urlcurrent = $urlseparate[3];
     <!-- JQUERY -->
     <script src="./../../assets/js/jquery-3.4.1.min.js" type="text/javascript"></script>
     
-    <script>  
-      //Si necesitas hacer algo con las respuestas del servidor
-      //hacelas aqui.
-      const handleReturnedData = (data) => {
-        /*
-        dcsnat = '<tr role="row">' +
-                  '<th scope="row">1</th>'+
-                  '<td role="cell">James</td>'+
-                  '<td role="cell">Matman</td>'+
-                  '<td role="cell">Chief Sandwich Eater</td>'+
-                  '<td role="cell">James</td>'+
-                  '<td role="cell">Matman</td>'+
-                  '<td role="cell">Chief Sandwich Eater</td>'+
-                  '<td role="cell">Chief Sandwich Eater</td>'+
-                '</tr>';*/
-        var dcsnat = JSON.parse(data);
-        ecsnat = '<tr role="row">';
-        ecsnat+='<th scope="row">1</th>';
-        var codComp;
-        if(dcsnat['data']['codComp'] == "08"){
-          codComp = 'N. DEBITO';
-        }
-        else{
-          codComp = 'NO EXISTE';
-        }
-        ecsnat+='<td role="cell">' + codComp + '</td>';
-        var numeroSerie = dcsnat['data']['numeroSerie'];
-        var numero = dcsnat['data']['numero'];
-        ecsnat+='<td role="cell">' + numeroSerie + ' - ' + numero + '</td>';
-        var fechaEmision = dcsnat['data']['fechaEmision'];
-        ecsnat+='<td role="cell">' + fechaEmision + '</td>';
-        var monto = dcsnat['data']['monto'];
-        ecsnat+='<td role="cell">' + monto + '</td>';
-        var estadoCp;
-        if(dcsnat['data']['estadoCp'] == "1"){
-          estadoCp = 'ACEPTADO';
-        }
-        else{
-          estadoCp = 'NO EXISTE';
-        }
-        ecsnat+='<td role="cell">' + estadoCp + '</td>';
-        var estadoRuc;
-        if(dcsnat['data']['estadoRuc'] == "00"){
-          estadoRuc = 'ACTIVO'
-        }
-        else{
-          estadoRuc = '-';
-        }
+<script>
+let prueba = <?php echo $jelsunat ?>;
+console.log(prueba);
+var contenido;
+function leerArchivo(e) {
+  var archivo = e.target.files[0];
+  if (!archivo) {
+    return;
+  }
+  var lector = new FileReader();
+  lector.onload = function(e) {
+    var contenido = e.target.result;
+    mostrarContenido(contenido);
+  };
+  lector.readAsText(archivo);
+}
+
+function mostrarContenido(contenido) {
+  var dseparador = '\n';
+  var detdocsep = '|';
+  var grudoc;
+  var dsunat = contenido.split(dseparador);
+  console.log(typeof dsunat);
+  //console.log(dsunat[5]);
+  //var dsunat = contenido.split(separador);  
+  for (var i=0; i < dsunat.length; i++) {
+    //console.log(dsunat[i]);
+    var ditemsunat = dsunat[i].split(detdocsep);
+    console.log(ditemsunat);
+    var documento = {
+      codComp: ditemsunat[1],
+      fechaEmision: ditemsunat[4],
+      monto: ditemsunat[5],
+      numRuc: ditemsunat[0],
+      numero: ditemsunat[3],
+      numeroSerie: ditemsunat[2]
+    };
+    console.log(documento);
+    //grudoc[i] = {documento};
+    
+   }
+   //console.log(grudoc);
+  var elemento = document.getElementById('contenido-archivo');
+  elemento.innerHTML = contenido;
+}
+
+document.getElementById('file-input').addEventListener('change', leerArchivo, false);
+  // Obtener referencia a botón
+  // Recuerda: el numeral o # indica id
+  const boton = document.querySelector("#btnConSunat");
+  // Agregar listener
+  boton.addEventListener("click", function(evento){
+    // Aquí todo el código que se ejecuta cuando se da click al botón
+    //Si necesitas hacer algo con las respuestas del servidor
+    //hacelas aqui.
+    const handleReturnedData = (data) => {
+      var ecsnat;
+      var ctcsunat;
+      var dcsnat = JSON.parse(data);
+      var codComp;
+      var numeroSerie = dcsnat['data']['numeroSerie'];
+      var numero = dcsnat['data']['numero'];
+      var fechaEmision = dcsnat['data']['fechaEmision'];
+      var monto = dcsnat['data']['monto'];
+      if(dcsnat['data']['codComp'] == "08"){
+        codComp = 'N. DEBITO';
+      }
+      else{
+        codComp = 'NO EXISTE';
+      }
+
+      var estadoCp;
+      if(dcsnat['data']['estadoCp'] == "1"){
+        estadoCp = 'ACEPTADO';
+        ctcsunat = 'class="table-success"';
+      }
+      else{
+        estadoCp = 'NO EXISTE';
+        ctcsunat = 'class="table-danger"';
+      }
+      ecsnat = '<tr ' + ctcsunat + ' role="row">';
+      ecsnat+='<th scope="row">1</th>';
+      ecsnat+='<td role="cell">' + codComp + '</td>';
+      ecsnat+='<td role="cell">' + numeroSerie + ' - ' + numero + '</td>';
+      ecsnat+='<td role="cell">' + fechaEmision + '</td>';
+      ecsnat+='<td role="cell">' + monto + '</td>';
+      ecsnat+='<td role="cell">' + estadoCp + '</td>';
+      var estadoRuc;
+      if(dcsnat['data']['estadoRuc'] == "00"){
+        estadoRuc = 'ACTIVO'
+      }
+      else{
+        estadoRuc = '-';
+      }
         ecsnat+='<td role="cell">' + estadoRuc + '</td>';
         var condDomiRuc;
         if(dcsnat['data']['condDomiRuc'] == "00"){
@@ -240,6 +246,8 @@ $urlcurrent = $urlseparate[3];
       //adata = JSON.parse(adata);
       console.log(typeof adata);
       makeRequests(adata);
+  });
+
    
     </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
